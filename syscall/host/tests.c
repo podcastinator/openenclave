916f3af4a53e98da9128941d17978b96725de418
Mike Brasher <mikbras@microsoft.com>
2020-01-09 16:18:53 -0600

    fix windows

--- syscall/host/tests.c
+++ syscall/host/tests.c
@@ -2,14 +2,7 @@
 // Licensed under the MIT License.
 
 #include <openenclave/internal/syscall/bits/tests.h>
-
-#if defined(__linux__)
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
-#elif defined(_WIN32)
-#include <windows.h>
-#endif
 
 void __oe_test_fail(const char* format, ...)
 {
